41b77fe063339b4b1b8df97d73f9684d42cdc484
stefv <svanpo@gmail.com>
2015-12-13 22:36:19 +0100

    Objets returned by ShaderToy.

---
+++ Windows/ShadertoyJson/RenderPassV1.cs
@@ -0,0 +1,63 @@
+﻿//
+// GLow screensaver
+// Copyright(C) Stéphane VANPOPERYNGHE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or(at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+using System.Collections.Generic;
+
+namespace GLow_Screensaver.Windows.ShadertoyJson
+{
+    /// <summary>
+    /// Render pass informations.
+    /// </summary>
+    public class RenderPassV1
+    {
+        //        "inputs":[{
+        //				"id":30,
+        //				"src":"\/presets\/tex16.png",
+        //				"ctype":"texture",
+        //				"channel":0,
+        //				"sampler":{
+        //					"filter":"mipmap",
+        //					"wrap":"repeat",
+        //					"vflip":"false",
+        //					"srgb":"false",
+        //					"internal":"byte"}
+        //}
+        //			],
+        //"outputs":[{
+        //	"channel":"0",
+        //	"dst":"-1"}
+        //],
+
+        /// <summary>
+        /// Source code of the shader.
+        /// </summary>
+        public string code { get; set; }
+
+        //     "name":"",
+        //"description":"",
+        //"type":"image"}
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public RenderPassV1()
+        {
+        }
+    }
+}
---
+++ Windows/ShadertoyJson/ShaderInfoV1.cs
@@ -0,0 +1,86 @@
+﻿//
+// GLow screensaver
+// Copyright(C) Stéphane VANPOPERYNGHE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or(at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+using System.Collections.Generic;
+
+namespace GLow_Screensaver.Windows.ShadertoyJson
+{
+    /// <summary>
+    /// Informations for one shader.
+    /// </summary>
+    public class ShaderInfoV1
+    {
+        /// <summary>
+        /// The ID of the shader.
+        /// </summary>
+        public string id { get; set; }
+
+        /// <summary>
+        /// The date.
+        /// </summary>
+        public long date { get; set; }
+
+        /// <summary>
+        /// Number of views.
+        /// </summary>
+        public long viewed { get; set; }
+
+        /// <summary>
+        /// The name.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// The username of the author.
+        /// </summary>
+        public string username { get; set; }
+
+        /// <summary>
+        /// The description.
+        /// </summary>
+        public string description { get; set; }
+
+        /// <summary>
+        /// Number of likes.
+        /// </summary>
+        public int likes { get; set; }
+
+        /// <summary>
+        /// Published of not.
+        /// </summary>
+        public int published { get; set; }
+
+        /// <summary>
+        /// Some flags.
+        /// </summary>
+        public int flags { get; set; }
+
+        /// <summary>
+        /// Tags.
+        /// </summary>
+        public List<string> tags { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ShaderInfoV1()
+        {
+        }
+    }
+}
