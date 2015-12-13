db567abb06ad51d2ca95c712c76adc49a687573d
stefv <svanpo@gmail.com>
2015-12-13 22:41:31 +0100

    Add objects from ShaderToy.

---
+++ Windows/ShadertoyJson/ListAll.cs
@@ -0,0 +1,46 @@
+﻿//
+// GLow screensaver
+// Copyright(C) Stéphane VANPOPERYNGHE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or(at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+using System.Collections.Generic;
+
+namespace GLow_Screensaver.Windows.ShadertoyJson
+{
+    /// <summary>
+    /// List with all the shaders from ShaderToy.
+    /// </summary>
+    public class ListAll
+    {
+        /// <summary>
+        /// The number of shaders.
+        /// </summary>
+        public int Shaders { get; set; }
+
+        /// <summary>
+        /// The list of shaders.
+        /// </summary>
+        public List<string> Results { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ListAll()
+        {
+        }
+    }
+}
---
+++ Windows/ShadertoyJson/ShaderV1.cs
@@ -0,0 +1,41 @@
+﻿//
+// GLow screensaver
+// Copyright(C) Stéphane VANPOPERYNGHE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or(at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+using System.Collections.Generic;
+
+namespace GLow_Screensaver.Windows.ShadertoyJson
+{
+    /// <summary>
+    /// The description of a shader from ShaderToy.
+    /// </summary>
+    public class ShaderV1
+    {
+        /// <summary>
+        /// The root of the shader.
+        /// </summary>
+        public ShaderRoot Shader { get; set; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ShaderV1()
+        {
+        }
+    }
+}
