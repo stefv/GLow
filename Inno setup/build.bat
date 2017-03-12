bd55c29e213ddbe202b9840cf2833d663bc93de2
stefv <svanpo@gmail.com>
2017-03-12 19:50:39 +0100

    Update the build script.

--- Inno setup/build.bat
+++ Inno setup/build.bat
@@ -1,11 +1,19 @@
+@echo off
 cls
 
+rem Check if the VS command prompt has been started
+if ("%VSINSTALLDIR%"=="") goto NOTSET
+set MSBUILD=%VSINSTALLDIR%\MSBuild\%VisualStudioVersion%\Bin
+
+rem Remove the previous EXE
+del glow*.exe
+
 rem Build the x86 version
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" "..\GLow Screensaver.sln" /t:Rebuild /p:Configuration=Release /p:Platform="x86"
+"%MSBUILD%\MsBuild.exe" "..\GLow Screensaver.sln" /t:Rebuild /p:Configuration=Release /p:Platform="x86"
 if ERRORLEVEL 1 exit /b 1
 
 rem Build the x64 version
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\MsBuild.exe" "..\GLow Screensaver.sln" /t:Rebuild /p:Configuration=Release /p:Platform="x64"
+"%MSBUILD%\MsBuild.exe" "..\GLow Screensaver.sln" /t:Rebuild /p:Configuration=Release /p:Platform="x64"
 if ERRORLEVEL 1 exit /b 1
 
 rem Build the x86 install
@@ -15,3 +23,9 @@ rem Build the x64 install
 "C:\Program Files (x86)\Inno Setup 5\iscc.exe" GLow.iss /DArch=x64
 
 exit /b 0
+
+:NOTSET
+echo The VSINSTALLDIR environment variable is not set.
+echo Did you start the Visual Studio command prompt ?
+
+exit /b 1
